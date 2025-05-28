0d91bd59c5d206be01f28ded9781c3c9b3ac7e54
petchdiamonddev <tanawat29664@gmail.com>
2025-05-28 21:51:05 +0700

    calculate
    คำนวณ เงินเดือน

---
+++ Template/calculate/testC68_boon/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace testC68_boon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.namef = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input1
+            // 
+            this.input1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.input1.Location = new System.Drawing.Point(291, 152);
+            this.input1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(171, 38);
+            this.input1.TabIndex = 0;
+            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
+            // 
+            // input2
+            // 
+            this.input2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.input2.Location = new System.Drawing.Point(291, 215);
+            this.input2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(171, 38);
+            this.input2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(412, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 271);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "คำนวณ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(292, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ABC";
+            // 
+            // namef
+            // 
+            this.namef.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.namef.Location = new System.Drawing.Point(291, 99);
+            this.namef.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.namef.Name = "namef";
+            this.namef.Size = new System.Drawing.Size(171, 38);
+            this.namef.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Location = new System.Drawing.Point(229, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ชื่อ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label5.Location = new System.Drawing.Point(168, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 31);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "เงินเดือน";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label6.Location = new System.Drawing.Point(156, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 31);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "เปอร์เซ็น";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.namef);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox namef;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Template/calculate/testC68_boon/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testC68_boon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // รับค่าจากกล่องข้อความ input1 แล้วเก็บไว้ในตัวแปร payin (เป็น string)
+            string payin = input1.Text;
+
+            // รับค่าจากกล่องข้อความ input2 แล้วเก็บไว้ในตัวแปร bonusin (เป็น string)
+            string bonusin = input2.Text;
+
+            // แปลงค่าจาก string (payin) ให้เป็นจำนวนเต็ม (int) แล้วเก็บไว้ในตัวแปร pay
+            int pay = Int32.Parse(payin);
+
+            // แปลงค่าจาก string (bonusin) ให้เป็นจำนวนเต็ม (int) แล้วเก็บไว้ในตัวแปร bonus
+            int bonus = Int32.Parse(bonusin);
+
+            // คำนวณเงินรวมที่ได้ = เงินเดือน + โบนัส
+            // โดยคำนวณโบนัสจากเปอร์เซ็นต์: (เงินเดือน * เปอร์เซ็นต์โบนัส / 100) แล้วบวกกับเงินเดือน
+            // ใช้ชนิด double เพื่อให้ได้ค่าทศนิยม
+            double sumpay = (pay * bonus / 100.0) + pay;
+
+            // คำนวณเฉพาะส่วนของโบนัส (เงินเดือน * เปอร์เซ็นต์โบนัส / 100)
+            double sumbonus = (pay * bonus / 100.0);
+
+            // แสดงผลรวมเงินทั้งหมด (เงินเดือน + โบนัส) ใน label1
+            // ใช้ ToString("F2") เพื่อให้แสดงทศนิยม 2 ตำแหน่ง (เช่น 1234.50)
+            label1.Text = sumpay.ToString("F2");
+
+            // แสดงเฉพาะโบนัสใน label2 โดยแปลงจาก double เป็น string
+            label2.Text = sumbonus.ToString();
+
+
+        }
+
+        private void input1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
